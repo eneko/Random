f78df58a97c8b38235a10cb73182a2b04b085fa3
Eneko Alonso <eneko.alonso@gmail.com>
2018-09-16 12:12:08 -0700

    Add shuffle command

---
+++ Sources/random/Commands/ShuffleCommand.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Utility
+import CommandRegistry
+import Logger
+
+class ShuffleCommand: Command, LineProcessorType {
+    let command = "shuffle"
+    let overview = "Shuffle input elements in random order (works with arguments and stdin)"
+
+    let options: PositionalArgument<[String]>
+
+    required init(parser: ArgumentParser) {
+        let subparser = parser.add(subparser: command, overview: overview)
+        options = subparser.add(positional: "options", kind: [String].self, optional: true, strategy: .upToNextOption, usage: "random shuffle optionA optionB...")
+    }
+
+    func run(with arguments: ArgumentParser.Result) throws {
+        let params = arguments.get(self.options) ?? []
+        let inputLines = splitFirstLineIfNeeded(lines: StandardInput.readAvailableLines())
+        var options = splitWordIfNeeded(words: params + inputLines)
+
+        options = Randomizer().shuffle(options: options)
+        Logger.standard.log(options)
+    }
+
+}
--- Sources/random/main.swift
+++ Sources/random/main.swift
@@ -3,4 +3,5 @@ import CommandRegistry
 var commands = CommandRegistry(usage: "<subcommand> <options>", overview: "A random command line tool 🎲")
 commands.register(command: DiceCommand.self)
 commands.register(command: PickCommand.self)
+commands.register(command: ShuffleCommand.self)
 commands.run()
